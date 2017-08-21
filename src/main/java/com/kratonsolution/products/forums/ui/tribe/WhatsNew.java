283700cff4426f0247ae9c1ee824559845f11ad7
ADP <agung.dodi.perdana@gmail.com>
2017-08-21 11:02:14 +0700

    update whatsnew

--- src/main/java/com/kratonsolution/products/forums/ui/tribe/WhatsNew.java
+++ src/main/java/com/kratonsolution/products/forums/ui/tribe/WhatsNew.java
@@ -3,6 +3,7 @@
  */
 package com.kratonsolution.products.forums.ui.tribe;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.data.domain.PageRequest;
@@ -12,9 +13,11 @@ import org.springframework.data.domain.Sort.Order;
 
 import com.kratonsolution.products.forums.common.Security;
 import com.kratonsolution.products.forums.common.Springs;
+import com.kratonsolution.products.forums.dm.TribeEvent;
 import com.kratonsolution.products.forums.dm.TribeEventRepository;
 import com.kratonsolution.products.forums.dm.TribeNews;
 import com.kratonsolution.products.forums.dm.TribeNewsRepository;
+import com.kratonsolution.products.forums.svc.TribeService;
 import com.kratonsolution.products.forums.ui.HomeContent;
 import com.kratonsolution.products.forums.ui.Icons;
 import com.vaadin.shared.ui.ContentMode;
@@ -34,7 +37,11 @@ public class WhatsNew extends VerticalLayout implements HomeContent
 	
 	private TribeEventRepository eventRepository = Springs.get(TribeEventRepository.class);
 	
+	private TribeService tribeService = Springs.get(TribeService.class);
+	
 	private HorizontalLayout newsLayout = new HorizontalLayout();
+	
+	private HorizontalLayout eventLayout = new HorizontalLayout();
 	
 	public WhatsNew()
 	{
@@ -52,6 +59,24 @@ public class WhatsNew extends VerticalLayout implements HomeContent
 		news.setWidth("100%");
 		news.addTab(newsLayout,"Tribe News",Icons.TRIBE_NEWS);
 		
+		eventLayout.setStyleName(ValoTheme.LAYOUT_HORIZONTAL_WRAPPING);
+		eventLayout.setMargin(true);
+		eventLayout.setSpacing(true);
+		
+		Accordion events = new Accordion();
+		events.setStyleName(ValoTheme.ACCORDION_BORDERLESS);
+		events.setWidth("100%");
+		events.addTab(eventLayout,"Tribe Event",Icons.TRIBE_EVENT);
+		
+		addComponent(news);
+		addComponent(events);
+		
+		initNews();
+		initEvent();
+	}
+
+	private void initNews()
+	{
 		List<TribeNews> tribeNews = newsRepository.findAllMyNews(new PageRequest(0, 50,new Sort(new Order(Direction.DESC,"timeCreated"))), Security.getUserEmail());
 		if(tribeNews.isEmpty())
 		{
@@ -66,13 +91,28 @@ public class WhatsNew extends VerticalLayout implements HomeContent
 				newsLayout.addComponent(new NewsDisplay(tnews));
 			});
 		}
+	}
+	
+	private void initEvent()
+	{
+		List<TribeEvent> events = new ArrayList<TribeEvent>();
 		
-		Accordion events = new Accordion();
-		events.setCaption("Tribe Event");
-		events.setIcon(Icons.TRIBE_EVENT);
-		events.setWidth("100%");
+		tribeService.findAllInvolved().forEach(tribe->{
+			events.addAll(eventRepository.findAllByTribe(tribe.getId()));
+		});
 		
-		addComponent(news);
-		addComponent(events);
+		if(events.isEmpty())
+		{
+			Label label = new Label(null, ContentMode.HTML);
+			label.setValue("<h3>No event.</h3>");
+		
+			eventLayout.addComponent(label);
+		}
+		else
+		{
+			events.forEach(event->{
+				eventLayout.addComponent(new EventDisplay(event));
+			});
+		}
 	}
 }
